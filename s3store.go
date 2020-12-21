ecaf2d28dba53bf6eab63752268b4ac9e7712b36
albrazeau <abrazeau@dewberry.com>
2020-12-21 11:56:13 -0500

    add doc comments

--- s3store.go
+++ s3store.go
@@ -15,34 +15,42 @@ import (
 	"github.com/aws/aws-sdk-go/service/s3"
 )
 
+// S3FileInfo is a wrapper around the s3.Object struct that implements the os.FileInfo interface
 type S3FileInfo struct {
 	s3 *s3.Object
 }
 
+// Name returns the file name of the s3 object
 func (obj *S3FileInfo) Name() string {
 	return *obj.s3.Key
 }
 
+// Size returns the file size in bytes
 func (obj *S3FileInfo) Size() int64 {
 	return *obj.s3.Size
 }
 
+// Mode defaults to Irregular
 func (obj *S3FileInfo) Mode() os.FileMode {
 	return os.ModeIrregular
 }
 
+// ModTime returns the time the s3 object was last modified
 func (obj *S3FileInfo) ModTime() time.Time {
 	return *obj.s3.LastModified
 }
 
+// IsDir returns a boolean determining whether an object is a directory or not
 func (obj *S3FileInfo) IsDir() bool {
 	return false
 }
 
+// Sys defaults to nil for objects of s3
 func (obj *S3FileInfo) Sys() interface{} {
 	return nil
 }
 
+// S3FSConfig stores the configuration and credentials necessary to create an s3 instance of the filestore
 type S3FSConfig struct {
 	S3Id     string
 	S3Key    string
@@ -50,12 +58,14 @@ type S3FSConfig struct {
 	S3Bucket string
 }
 
+// S3FS satisfies the FileStore interface, allowing for generic file operations to be done on s3 blobs
 type S3FS struct {
 	session *session.Session
 	config  *S3FSConfig
 	maxKeys int64
 }
 
+// GetDir is similar to an ls unix call. It lists the objects at an s3 prefix, with the option of being recursive
 func (s3fs *S3FS) GetDir(path string, recursive bool) (*[]FileStoreResultObject, error) {
 	s3Path := strings.Trim(path, "/") + "/"
 	var delim string
@@ -116,6 +126,7 @@ func (s3fs *S3FS) GetDir(path string, recursive bool) (*[]FileStoreResultObject,
 	return &result, nil
 }
 
+// GetObject will return the body of an s3 object as a ReadCloser, meaning it has the basic Read and Close methods
 func (s3fs *S3FS) GetObject(path string) (io.ReadCloser, error) {
 	s3Path := strings.TrimPrefix(path, "/")
 	svc := s3.New(s3fs.session)
@@ -127,6 +138,7 @@ func (s3fs *S3FS) GetObject(path string) (io.ReadCloser, error) {
 	return output.Body, err
 }
 
+// PutObject will take the data provided and put it on s3 at the path provided
 func (s3fs *S3FS) PutObject(path string, data []byte) (*FileOperationOutput, error) {
 	s3Path := strings.TrimPrefix(path, "/")
 	svc := s3.New(s3fs.session)
@@ -143,6 +155,7 @@ func (s3fs *S3FS) PutObject(path string, data []byte) (*FileOperationOutput, err
 	return &FileOperationOutput{Md5: *s3output.ETag}, nil
 }
 
+// DeleteObjects will take one or more paths, and delete them from the s3 file system
 func (s3fs *S3FS) DeleteObjects(path ...string) error {
 	svc := s3.New(s3fs.session)
 	objects := make([]*s3.ObjectIdentifier, 0, len(path))
@@ -232,6 +245,7 @@ func (s3fs *S3FS) CompleteObjectUpload(u CompletedObjectUploadConfig) error {
 	return err
 }
 
+// Walk will traverse an s3 file system recursively, starting at the provided prefix, and apply the visitorFunction to each s3 object
 func (s3fs *S3FS) Walk(path string, vistorFunction FileVisitFunction) error {
 	s3Path := strings.TrimPrefix(path, "/")
 	s3delim := ""
@@ -265,6 +279,8 @@ func (s3fs *S3FS) Walk(path string, vistorFunction FileVisitFunction) error {
 /*
   these functions are not part of the filestore interface and are unique to the S3FS
 */
+
+// SharedAccessURL will create a presigned url that can be used to access/download an object from an s3 bucket. It will only be valid for the duration specified
 func (s3fs *S3FS) SharedAccessURL(path string, expiration time.Duration) (string, error) {
 	s3Path := strings.TrimPrefix(path, "/")
 	svc := s3.New(s3fs.session)
@@ -276,6 +292,7 @@ func (s3fs *S3FS) SharedAccessURL(path string, expiration time.Duration) (string
 	return req.Presign(expiration)
 }
 
+// SetObjectPublic will change the acl permissions on an s3 object and make it publically readable
 func (s3fs *S3FS) SetObjectPublic(path string) (string, error) {
 	s3Path := strings.TrimPrefix(path, "/")
 	svc := s3.New(s3fs.session)
